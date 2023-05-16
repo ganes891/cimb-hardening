cf17b3b06cf79feedbfd9ea3035360c4d168a8ed
ganes891 <ganesan.kandasami@gmail.com>
2023-05-16 21:51:28 +0800

    new file sec6.0

--- roles/os_hardening_copy/files/commands.sh
+++ roles/os_hardening_copy/files/commands.sh
@@ -3,8 +3,8 @@ AUDITDIR="/var/tmp/$(hostname -s)_audit"
 TIME="$(date +%F_%T)"
 . /etc/os-release
 MAIN_VERSION_ID="$(echo ${VERSION_ID} |cut -f1 -d'.')"
-if [[ ${MAIN_VERSION_ID} -lt 8 ]]; then
-  echo "OS release lower than 8 not supported. You are running ${VERSION_ID}"
+if [[ ${MAIN_VERSION_ID} -lt 9 ]]; then
+  echo "OS release lower than 9 not supported. You are running ${VERSION_ID}"
 fi
 
 mkdir -p $AUDITDIR
