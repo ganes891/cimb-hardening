cafad6435ecc85434a5b5cd93335691c0dd51b53
ganes891 <ganesan.kandasami@gmail.com>
2023-05-16 22:27:15 +0800

    new file sec6.0

--- roles/os_hardening_copy/files/commands.sh
+++ roles/os_hardening_copy/files/commands.sh
@@ -43,3 +43,35 @@ YOUR_COMPANY_NAME AUTHORIZED USE ONLY
 EOF
 rm -rf /etc/issue
 ln -s /etc/issue.net /etc/issue
+
+##################network-changes##################
+cat > /etc/sysctl.d/99-sysctl.conf << EOF
+net.ipv4.ip_forward=0
+net.ipv4.conf.all.send_redirects=0
+net.ipv4.conf.default.send_redirects=0
+net.ipv4.conf.all.accept_source_route=0
+net.ipv4.conf.default.accept_source_route=0
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default.accept_redirects=0
+net.ipv4.conf.all.secure_redirects=0
+net.ipv4.conf.default.secure_redirects=0
+net.ipv4.conf.all.log_martians=1
+net.ipv4.conf.default.log_martians=1
+net.ipv4.icmp_echo_ignore_broadcasts=1
+net.ipv4.icmp_ignore_bogus_error_responses=1
+net.ipv4.icmp_echo_bold_ignore_broadcasts=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.tcp_syncookies=1
+net.ipv6.conf.all.accept_ra=0
+net.ipv6.conf.default.accept_ra=0
+net.ipv6.conf.all.accept_redirects=0
+net.ipv6.conf.default.accept_redirects=0
+net.ipv6.conf.all.disable_ipv6=1
+fs.suid_dumpable=0
+EOF
+
+###############
+
+
+
