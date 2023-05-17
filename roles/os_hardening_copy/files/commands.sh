a69b78d5b4e14035dc3178ccc63f3e860ba86bfa
ganes891 <ganesan.kandasami@gmail.com>
2023-05-17 14:41:46 +0800

    new changes

--- roles/os_hardening_copy/files/commands.sh
+++ roles/os_hardening_copy/files/commands.sh
@@ -39,12 +39,12 @@ cat > /etc/issue.net << 'EOF'
 EOF
 cp -p /etc/motd /etc/motd_$TIME.bak
 cat > /etc/motd << 'EOF'
-YOUR_COMPANY_NAME AUTHORIZED USE ONLY
+RHB Bank AUTHORIZED USE ONLY
 EOF
 rm -rf /etc/issue
 ln -s /etc/issue.net /etc/issue
 
-##################network-changes##################
+#########################network-changes#####################################
 cat > /etc/sysctl.d/99-sysctl.conf << EOF
 net.ipv4.ip_forward=0
 net.ipv4.conf.all.send_redirects=0
@@ -109,4 +109,282 @@ awk -F: '($1!="root" && $1!="sync" &&  $1!="shutdown" && $1!="halt" && $1!~/^\+/
 
 echo "Ensure system accounts are secured second command - SEC - 5.4.5"
 awk -F: '($1!="root" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"') {print $1}' /etc/passwd | xargs -I '{}' passwd -S '{}' | awk '($2!="L" && $2!="LK") {print $1}' 
+
+#################################SEC- 6.2 #################################################
+############################User and Group Settings########################################
+
+echo "Reviewing User and Group Settings..."
+echo "Reviewing User and Group Settings..." >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/passwd >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/shadow >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/group >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/cat /etc/passwd | /bin/awk -F: '($3 == 0) { print $1 }' >> $AUDITDIR/reviewusrgrp_$TIME.log
+
+echo "Setting Sticky Bit on All World-Writable Directories..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null | xargs chmod a+t >> $AUDITDIR/sticky_on_world_$TIME.log
+
+echo "Searching for world writable files..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002 >> $AUDITDIR/world_writable_files_$TIME.log
+
+echo "Searching for Un-owned files and directories..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -nouser -ls >> $AUDITDIR/unowned_files_$TIME.log
+
+#34: Find Un-grouped Files and Directories
+echo "Searching for Un-grouped files and directories..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -nogroup -ls >> $AUDITDIR/ungrouped_files_$TIME.log
+
+echo "Searching for SUID System Executables..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -4000 -print >> $AUDITDIR/suid_exec_$TIME.log
+
+echo "Searching for SGID System Executables..."
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -2000 -print >> $AUDITDIR/sgid_exec_$TIME.log
+
+echo "Searching for empty password fields..."
+/bin/cat /etc/shadow | /bin/awk -F: '($2 == "" ) { print $1 " does not have a password "}' >> $AUDITDIR/empty_passwd_$TIME.log
+
+echo "Reviewing User and Group Settings..."
+echo "Reviewing User and Group Settings..." >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/passwd >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/shadow >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/grep '^+:' /etc/group >> $AUDITDIR/reviewusrgrp_$TIME.log
+/bin/cat /etc/passwd | /bin/awk -F: '($3 == 0) { print $1 }' >> $AUDITDIR/reviewusrgrp_$TIME.log
+
+echo "Checking root PATH integrity..."
+
+if [ "`echo $PATH | /bin/grep :: `" != "" ]; then
+    echo "Empty Directory in PATH (::)" >> $AUDITDIR/root_path_$TIME.log
+fi
+
+if [ "`echo $PATH | /bin/grep :$`"  != "" ]; then
+    echo "Trailing : in PATH" >> $AUDITDIR/root_path_$TIME.log
+fi
+
+p=`echo $PATH | /bin/sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'`
+set -- $p
+while [ "$1" != "" ]; do
+    if [ "$1" = "." ]; then
+        echo "PATH contains ." >> $AUDITDIR/root_path_$TIME.log
+        shift
+        continue
+    fi
+    if [ -d $1 ]; then
+        dirperm=`/bin/ls -ldH $1 | /bin/cut -f1 -d" "`
+        if [ `echo $dirperm | /bin/cut -c6 ` != "-" ]; then
+            echo "Group Write permission set on directory $1" >> $AUDITDIR/root_path_$TIME.log
+        fi
+        if [ `echo $dirperm | /bin/cut -c9 ` != "-" ]; then
+            echo "Other Write permission set on directory $1" >> $AUDITDIR/root_path_$TIME.log
+        fi
+            dirown=`ls -ldH $1 | awk '{print $3}'`
+           if [ "$dirown" != "root" ] ; then
+             echo "$1 is not owned by root" >> $AUDITDIR/root_path_$TIME.log
+              fi
+    else
+            echo "$1 is not a directory" >> $AUDITDIR/root_path_$TIME.log
+      fi
+    shift
+done
+
+echo "Checking Permissions on User Home Directories..."
+
+for dir in `/bin/cat /etc/passwd  | /bin/egrep -v '(root|halt|sync|shutdown)' |\
+    /bin/awk -F: '($8 == "PS" && $7 != "/sbin/nologin") { print $6 }'`; do
+        dirperm=`/bin/ls -ld $dir | /bin/cut -f1 -d" "`
+        if [ `echo $dirperm | /bin/cut -c6 ` != "-" ]; then
+            echo "Group Write permission set on directory $dir" >> $AUDITDIR/home_permission_$TIME.log
+        fi
+        if [ `echo $dirperm | /bin/cut -c8 ` != "-" ]; then
+            echo "Other Read permission set on directory $dir" >> $AUDITDIR/home_permission_$TIME.log
+
+        fi
+
+        if [ `echo $dirperm | /bin/cut -c9 ` != "-" ]; then
+            echo "Other Write permission set on directory $dir" >> $AUDITDIR/home_permission_$TIME.log
+        fi
+        if [ `echo $dirperm | /bin/cut -c10 ` != "-" ]; then
+            echo "Other Execute permission set on directory $dir" >> $AUDITDIR/home_permission_$TIME.log
+        fi
+done
+
+echo "Checking User Dot File Permissions..."
+for dir in `/bin/cat /etc/passwd | /bin/egrep -v '(root|sync|halt|shutdown)' |
+/bin/awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do
+    for file in $dir/.[A-Za-z0-9]*; do
+
+        if [ ! -h "$file" -a -f "$file" ]; then
+            fileperm=`/bin/ls -ld $file | /bin/cut -f1 -d" "`
+
+            if [ `echo $fileperm | /bin/cut -c6 ` != "-" ]; then
+                echo "Group Write permission set on file $file" >> $AUDITDIR/dotfile_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c9 ` != "-" ]; then
+                echo "Other Write permission set on file $file" >> $AUDITDIR/dotfile_permission_$TIME.log
+            fi
+        fi
+
+    done
+
+done
+
+echo "Checking Permissions on User .netrc Files..."
+for dir in `/bin/cat /etc/passwd | /bin/egrep -v '(root|sync|halt|shutdown)' |\
+    /bin/awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do
+    for file in $dir/.netrc; do
+        if [ ! -h "$file" -a -f "$file" ]; then
+            fileperm=`/bin/ls -ld $file | /bin/cut -f1 -d" "`
+            if [ `echo $fileperm | /bin/cut -c5 ` != "-" ]
+            then
+                echo "Group Read set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c6 ` != "-" ]
+            then
+                echo "Group Write set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c7 ` != "-" ]
+            then
+                echo "Group Execute set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c8 ` != "-" ]
+            then
+                echo "Other Read  set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c9 ` != "-" ]
+            then
+                echo "Other Write set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+            if [ `echo $fileperm | /bin/cut -c10 ` != "-" ]
+            then
+                echo "Other Execute set on $file" >> $AUDITDIR/netrd_permission_$TIME.log
+            fi
+        fi
+    done
+done
+
+echo "Checking for Presence of User .rhosts Files..."
+for dir in `/bin/cat /etc/passwd | /bin/egrep -v '(root|halt|sync|shutdown)' |\
+    /bin/awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do
+    for file in $dir/.rhosts; do
+        if [ ! -h "$file" -a -f "$file" ]; then
+            echo ".rhosts file in $dir" >> $AUDITDIR/rhosts_$TIME.log
+        fi    done
+done
+
+echo "Checking Groups in /etc/passwd..."
+
+for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do
+  grep -q -P "^.*?:x:$i:" /etc/group
+  if [ $? -ne 0 ]; then
+    echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group" >> $AUDITDIR/audit_$TIME.log
+  fi
+done
+
+echo "Checking That Users Are Assigned Home Directories..."
+
+cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read user uid dir; do
+  if [ $uid -ge 500 -a ! -d "$dir" -a $user != "nfsnobody" ]; then
+ echo "The home directory ($dir) of user $user does not exist." >> $AUDITDIR/audit_$TIME.log
+ fi
+done
+
+echo "Checking That Defined Home Directories Exist..."
+
+cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read user uid dir; do
+ if [ $uid -ge 500 -a -d "$dir" -a $user != "nfsnobody" ]; then
+ owner=$(stat -L -c "%U" "$dir")
+ if [ "$owner" != "$user" ]; then
+ echo "The home directory ($dir) of user $user is owned by $owner." >> $AUDITDIR/audit_$TIME.log
+ fi
+ fi
+done
+
+echo "Checking for Duplicate UIDs..."
+
+/bin/cat /etc/passwd | /bin/cut -f3 -d":" | /bin/sort -n | /usr/bin/uniq -c |\
+    while read x ; do
+    [ -z "${x}" ] && break
+    set - $x
+    if [ $1 -gt 1 ]; then
+        users=`/bin/gawk -F: '($3 == n) { print $1 }' n=$2 \
+            /etc/passwd | /usr/bin/xargs`
+        echo "Duplicate UID ($2): ${users}" >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
+echo "Checking for Duplicate GIDs..."
+
+/bin/cat /etc/group | /bin/cut -f3 -d":" | /bin/sort -n | /usr/bin/uniq -c |\
+    while read x ; do
+    [ -z "${x}" ] && break
+    set - $x
+    if [ $1 -gt 1 ]; then
+        grps=`/bin/gawk -F: '($3 == n) { print $1 }' n=$2 \
+            /etc/group | xargs`
+        echo "Duplicate GID ($2): ${grps}" >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
+echo "Checking That Reserved UIDs Are Assigned to System Accounts..."
+
+defUsers="root bin daemon adm lp sync shutdown halt mail news uucp operator games
+gopher ftp nobody nscd vcsa rpc mailnull smmsp pcap ntp dbus avahi sshd rpcuser
+nfsnobody haldaemon avahi-autoipd distcache apache oprofile webalizer dovecot squid
+named xfs gdm sabayon usbmuxd rtkit abrt saslauth pulse postfix tcpdump"
+/bin/cat /etc/passwd | /bin/awk -F: '($3 < 500) { print $1" "$3 }' |\
+    while read user uid; do
+        found=0
+        for tUser in ${defUsers}
+        do
+            if [ ${user} = ${tUser} ]; then
+                found=1
+            fi
+        done
+        if [ $found -eq 0 ]; then
+            echo "User $user has a reserved UID ($uid)."  >> $AUDITDIR/audit_$TIME.log
+        fi
+    done
+
+echo "Checking for Duplicate User Names..."
+
+cat /etc/passwd | cut -f1 -d":" | sort -n | /usr/bin/uniq -c |\
+    while read x ; do
+    [ -z "${x}" ] && break
+    set - $x
+    if [ $1 -gt 1 ]; then
+        uids=`/bin/gawk -F: '($1 == n) { print $3 }' n=$2 \
+            /etc/passwd | xargs`
+        echo "Duplicate User Name ($2): ${uids}"  >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
+echo "Checking for Duplicate Group Names..."
+
+cat /etc/group | cut -f1 -d":" | /bin/sort -n | /usr/bin/uniq -c |\
+    while read x ; do
+    [ -z "${x}" ] && break
+    set - $x
+    if [ $1 -gt 1 ]; then
+        gids=`/bin/gawk -F: '($1 == n) { print $3 }' n=$2 \
+            /etc/group | xargs`
+        echo "Duplicate Group Name ($2): ${gids}"  >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
+echo "Checking for Presence of User .netrc Files..."
+
+for dir in `/bin/cat /etc/passwd |\
+    /bin/awk -F: '{ print $6 }'`; do
+    if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
+        echo ".netrc file $dir/.netrc exists"  >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
+echo "Checking for Presence of User .forward Files..."
+
+for dir in `/bin/cat /etc/passwd |\
+    /bin/awk -F: '{ print $6 }'`; do
+    if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then
+        echo ".forward file $dir/.forward exists"  >> $AUDITDIR/audit_$TIME.log
+    fi
+done
+
 
