615dbe405d48c7ff79e3cad6712d4174f14e54d0
ganes891 <ganesan.kandasami@gmail.com>
2023-05-17 13:07:41 +0800

    new changes

--- roles/os_hardening_copy/files/commands.sh
+++ roles/os_hardening_copy/files/commands.sh
@@ -103,5 +103,10 @@ done
 chown root:root /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config
 
-#################################################################################
+###############################SEC- 5.4.5#################################################
+echo "Ensure system accounts are secured  - SEC - 5.4.5 "
+awk -F: '($1!="root" && $1!="sync" &&  $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false") {print}' /etc/passwd  
+
+echo "Ensure system accounts are secured second command - SEC - 5.4.5"
+awk -F: '($1!="root" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"') {print $1}' /etc/passwd | xargs -I '{}' passwd -S '{}' | awk '($2!="L" && $2!="LK") {print $1}' 
 
