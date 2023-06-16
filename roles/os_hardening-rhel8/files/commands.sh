46691287002fb04f0366cfeaa956e2ef4efbfc80
ganes891 <ganesan.kandasami@gmail.com>
2023-06-16 15:34:38 +0800

    test

--- roles/os_hardening-rhel8/files/commands.sh
+++ roles/os_hardening-rhel8/files/commands.sh
@@ -3,7 +3,7 @@ AUDITDIR="/var/tmp/$(hostname -s)_audit"
 TIME="$(date +%F_%T)"
 . /etc/os-release
 MAIN_VERSION_ID="$(echo ${VERSION_ID} |cut -f1 -d'.')"
-if [[ ${MAIN_VERSION_ID} -lt 9 ]]; then
+if [[ ${MAIN_VERSION_ID} -lt 8 ]]; then
   echo "OS release lower than 8 not supported. You are running ${VERSION_ID}"
 fi
 
